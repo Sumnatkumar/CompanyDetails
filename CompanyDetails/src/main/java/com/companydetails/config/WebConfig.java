5b112cdcdcd5e3e3a879425b153f1533fbbcb80c
Sumant  kumar <135256285+Sumnatkumar@users.noreply.github.com>
2025-11-20 22:06:05 +0530

    companydriverdetails

---
+++ CompanyDetails/src/main/java/com/companydetails/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.companydetails.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+public class WebConfig {
+}
